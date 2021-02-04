f0e5962185d55dc3d340955e34b0a90fda7d6a10
Larry Clement <lclement@calbaptist.edu>
2021-02-04 10:48:28 -0800

    Initial Commit After Class on 2/4/2021

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str1 = new String("Hello");
+        String str2 = new String("hello");
+
+        System.out.println(str1.equalsIgnoreCase(str2));
+    }
+}
